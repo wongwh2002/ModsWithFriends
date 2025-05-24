e098504c12a9b1092cd1630d2630042fe04d913b
NigelYeoTW <nigelyeotongwei@gmail.com>
2025-05-24 19:33:07 +0800

    Add buttons and sessions to header

--- modwithfriends/src/Header.js
+++ modwithfriends/src/Header.js
@@ -5,7 +5,16 @@ import logo from './assets/MWF_logo.png';
 function Header() {
   return (
     <div className='header'>
-      <img src={logo}></img>
+      <img src={logo} className='logo'></img>
+      <div className='session-id'>
+        <p className='bold'>
+          Session ID:
+        </p>
+      </div>
+      <div className='header-buttons'>
+        <button className='session'>New Session</button>
+        <button className='session'>Join Session</button>
+      </div>
     </div>
   )
 }
