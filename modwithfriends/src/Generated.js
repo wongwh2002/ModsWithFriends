38657f166a220ed31536321ec39cddf51a31e551
NigelYeoTW <nigelyeotongwei@gmail.com>
2025-06-05 10:52:42 +0800

    Add generation page outline
    Require backend and algo to link before inputting mroe design

--- modwithfriends/src/Generated.js
+++ modwithfriends/src/Generated.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './Generated.css'
+import GeneratedTimetable from './GeneratedTimetable';
 
 function Generated() {
+
+  const timetables = [1,2,3,4,5];
+
   return (
-    <></>
+    <div className='generated-wrapper'>
+      <div className='generated-container'>
+        {timetables.map(timetable => {
+          return <GeneratedTimetable/>
+        })}
+      </div>
+    </div>
   )
 }
 
---
+++ modwithfriends/src/GeneratedTimetable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './GeneratedTimetable.css';
+
+function GeneratedTimetable() {
+  return (
+    <div className='gt-container'>
+    </div>
+  )
+}
+
+export default GeneratedTimetable;
