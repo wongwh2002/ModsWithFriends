5e306a29950faff4bfb0f888614f3a698fe550c1
NigelYeoTW <nigelyeotongwei@gmail.com>
2025-06-18 23:39:52 +0800

    Pretify the joining of strings
    TODO: Empty selection creation of room

--- modwithfriends/src/RoomCard.js
+++ modwithfriends/src/RoomCard.js
@@ -4,10 +4,10 @@ import './RoomCard.css';
 function RoomCard({roomInfo}) {
   return (
     <div className='room-card-container'>
-      <p className='mod-title'>{roomInfo["modules"].toString()}</p>
+      <p className='mod-title'>{roomInfo["modules"].join(", ")}</p>
       <div className='participants-wrapper'>
         <div className='participants-container'>
-          <p>{roomInfo["users"].toString()}</p>
+          <p>{roomInfo["users"].join(", ")}</p>
         </div>
         <button className='room-action'>Join</button>
       </div>
