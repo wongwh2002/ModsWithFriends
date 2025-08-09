c25dea4ba3f6cfa37ed65041f04458aefab96672
NigelYeoTW <nigelyeotongwei@gmail.com>
2025-08-09 13:37:35 +0800

    Change port

--- Server/server.py
+++ Server/server.py
@@ -253,5 +253,5 @@ def serve_file(filename):
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 4000))
+    port = int(os.getenv("PORT", 4001))
     app.run(host="0.0.0.0", port=port)
