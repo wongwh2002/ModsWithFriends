764356e7611758c0d9d3070318290f3caa416db3
NigelYeoTW <nigelyeotongwei@gmail.com>
2025-08-11 21:48:27 +0800

    Implement copy share link button

--- modwithfriends/src/Header.js
+++ modwithfriends/src/Header.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { useNavigate } from "react-router-dom";
 import logo from './assets/MWF_logo.png';
-import { Link } from 'react-router-dom';
+import clipboard from './assets/copy.png';
 import { useStateContext } from './Context';
 
 function Header({setCreateSession, setJoinSession, setShareSession, setBody, body}) {
 
   const { setOpenFromHeader } = useStateContext();
+  const [copyDescription, setCopyDescription] = useState(false);
+  const [copyMessage, setCopyMessage] = useState("Copy Share Link");
 
   const navigate = useNavigate();
 
@@ -18,6 +20,11 @@ function Header({setCreateSession, setJoinSession, setShareSession, setBody, bod
   const joinSession = () => {
     setOpenFromHeader(true);
     setJoinSession(true);
+  }
+
+  const handleCopy = () => {
+    setCopyMessage("Copied!");
+    navigator.clipboard.writeText(window.location.href);
   }
 
   const shareSession = () => {
@@ -36,11 +43,23 @@ function Header({setCreateSession, setJoinSession, setShareSession, setBody, bod
         <p className='bold'>
           Session ID: {body}
         </p>
+        <div className='copy-wrapper'>
+          {body === '-' ? <></> : 
+          <img src={clipboard} className='clipboard-icon' 
+            onMouseEnter={() => setCopyDescription(true)} onMouseLeave={() => {setCopyDescription(false); setCopyMessage("Copy Share Link");}}
+            onClick={handleCopy} />
+          }
+          {copyDescription ? 
+          <div className='copy-description'>
+            <p className='no-margin'>{copyMessage}</p>
+          </div> : <></>
+          }
+        </div>
       </div>
       <div className='header-buttons'>
         <button className='session' onClick={createSession}>New Session</button>
         <button className='session' onClick={joinSession}>Join Session</button>
-        {body === '-' ? <></> : <button className='session' onClick={shareSession}>Share Session</button>}
+        {/*body === '-' ? <></> : <button className='session' onClick={shareSession}>Share Session</button>*/}
       </div>
     </div>
   )
